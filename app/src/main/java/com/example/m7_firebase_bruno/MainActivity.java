7d24473938af4e3d40e1462a354b2536deb59262
Bruno Amat García <brunoamagar@campus.monlau.com>
2023-02-03 15:43:47 +0100

    Initial commit

---
+++ app/src/main/java/com/example/m7_firebase_bruno/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.m7_firebase_bruno;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatabaseReference myRef = database.getReference();
+        myRef.child("/1234/Grupo1/1").setValue("Hola");
+
+    }
+}
